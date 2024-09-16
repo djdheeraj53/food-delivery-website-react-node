185842ab6febe91afd3b81390ebcbc084209b139
Dheeraj Kumar <dheerajkumar@Dheerajs-MacBook-Pro.local>
2024-09-16 20:48:36 +0530

    header menu routes design

---
+++ frontend/src/assets/assets.tsx
@@ -0,0 +1,128 @@
+import search_icon from './../assets/frontend_assets/search_icon.png';
+import logo from './../assets/frontend_assets/logo.png';
+import basket_icon from './../assets/frontend_assets/basket_icon.png';
+import header_img from './../assets/frontend_assets/header_img.png';
+
+import menu_1 from './../assets/frontend_assets/menu_1.png';
+import menu_2 from './../assets/frontend_assets/menu_2.png';
+import menu_3 from './../assets/frontend_assets/menu_3.png';
+import menu_4 from './../assets/frontend_assets/menu_4.png';
+import menu_5 from './../assets/frontend_assets/menu_5.png';
+import menu_6 from './../assets/frontend_assets/menu_6.png';
+import menu_7 from './../assets/frontend_assets/menu_7.png';
+import menu_8 from './../assets/frontend_assets/menu_8.png';
+
+import food_1 from './../assets/frontend_assets/food_1.png';
+import food_2 from './../assets/frontend_assets/food_2.png';
+import food_3 from './../assets/frontend_assets/food_3.png';
+import food_4 from './../assets/frontend_assets/food_4.png';
+import food_5 from './../assets/frontend_assets/food_5.png';
+import food_6 from './../assets/frontend_assets/food_6.png';
+import food_7 from './../assets/frontend_assets/food_7.png';
+import food_8 from './../assets/frontend_assets/food_8.png';
+import food_9 from './../assets/frontend_assets/food_9.png';
+import food_10 from './../assets/frontend_assets/food_10.png';
+import food_11 from './../assets/frontend_assets/food_11.png';
+import food_12 from './../assets/frontend_assets/food_12.png';
+import food_13 from './../assets/frontend_assets/food_13.png';
+import food_14 from './../assets/frontend_assets/food_14.png';
+import food_15 from './../assets/frontend_assets/food_15.png';
+import food_16 from './../assets/frontend_assets/food_16.png';
+import food_17 from './../assets/frontend_assets/food_17.png';
+import food_18 from './../assets/frontend_assets/food_18.png';
+import food_19 from './../assets/frontend_assets/food_19.png';
+import food_20 from './../assets/frontend_assets/food_20.png';
+import food_21 from './../assets/frontend_assets/food_21.png';
+import food_22 from './../assets/frontend_assets/food_22.png';
+import food_23 from './../assets/frontend_assets/food_23.png';
+import food_24 from './../assets/frontend_assets/food_24.png';
+import food_25 from './../assets/frontend_assets/food_25.png';
+import food_26 from './../assets/frontend_assets/food_26.png';
+import food_27 from './../assets/frontend_assets/food_27.png';
+import food_28 from './../assets/frontend_assets/food_28.png';
+import food_29 from './../assets/frontend_assets/food_29.png';
+import food_30 from './../assets/frontend_assets/food_30.png';
+import food_31 from './../assets/frontend_assets/food_31.png';
+import food_32 from './../assets/frontend_assets/food_32.png';
+
+export const assests = {
+  logo,
+  basket_icon,
+  search_icon,
+  header_img,
+  menu_1,
+  menu_2,
+  menu_3,
+  menu_4,
+  menu_5,
+  menu_6,
+  menu_7,
+  menu_8,
+  food_1,
+  food_2,
+  food_3,
+  food_4,
+  food_5,
+  food_6,
+  food_7,
+  food_8,
+  food_9,
+  food_10,
+  food_11,
+  food_12,
+  food_13,
+  food_14,
+  food_15,
+  food_16,
+  food_17,
+  food_18,
+  food_19,
+  food_20,
+  food_21,
+  food_22,
+  food_23,
+  food_24,
+  food_25,
+  food_26,
+  food_27,
+  food_28,
+  food_29,
+  food_30,
+  food_31,
+  food_32
+};
+
+export const menu_list = [
+  {
+    menu_name: "Salad",
+    menu_image: menu_1
+  },
+  {
+    menu_name: "Rolls",
+    menu_image: menu_2
+  },
+  {
+    menu_name: "Deserts",
+    menu_image: menu_3
+  },
+  {
+    menu_name: "Sandwich",
+    menu_image: menu_4
+  },
+  {
+    menu_name: "Salad",
+    menu_image: menu_5
+  },
+  {
+    menu_name: "Salad",
+    menu_image: menu_6
+  },
+  {
+    menu_name: "Salad",
+    menu_image: menu_7
+  },
+  {
+    menu_name: "Salad",
+    menu_image: menu_8
+  }
+]
